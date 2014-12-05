8d34d01981a686c53f1a6ae037bf9f826649807f
Dmitry Sadakov <sadakov@gmail.com>
2014-12-05 15:00:47 -0500

    Adding mixpanel and yandex.metrika

--- app/scripts/ga.js
+++ app/scripts/ga.js
@@ -3,7 +3,7 @@
 /* exported: trackEvent*/
 /* jshint ignore:start */
 
-//Google Analytics
+// Google Analytics
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -12,6 +12,19 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-32350099-1', 'auto');
 ga('require', 'displayfeatures');
 ga('send', 'pageview');
+
+// Yandex.Metrika counter from http://mc.yandex.ru/metrika/watch.js
+var yaCounter27385172 = new Ya.Metrika({id:27385172,
+          webvisor:true,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          trackHash:true});
+
+// mix panel from cdn.mxpnl.com
+(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+mixpanel.init("2d7b99b5dbf7b6e6d7940a91a7f0d973");
 
 
 function trackEvent(category, action, label, value, nonInteraction) {
